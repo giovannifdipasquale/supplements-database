8f74da6dd78f0bb3fa2bfb020802fcbdf19fd639
giovannifdipasquale <giovanni.francesco97@gmail.com>
2026-02-04 20:23:49 +0100

    added page myList

--- src/pages/MyList/MyList.jsx
+++ src/pages/MyList/MyList.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Table from 'src/components/Table/Table'
+import { MySupplementsContext } from 'src/context/MySupplementsContext'
+import supplements from 'public/supplements.json'
 
 function MyList() {
+  const { mySupplements } = useContext(MySupplementsContext);
+  const supplementsArr = mySupplements.map((id) => {
+    return supplements.find((supplement) => supplement.id === id);
+  });
+
   return (
-    <Table />
+    <Table supplements={supplementsArr} />
   )
 }
 
